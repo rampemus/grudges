6c99bcc9b0b98560a919377a2324b53ee06fd9d6
rampemus <pasi.toivanen@live.com>
2020-06-02 20:00:51 +0300

    AWS amplify plugged in

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
+import Amplify from 'aws-amplify';
+import configuration from './aws-exports';
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+Amplify.configure(configuration)
+
+ReactDOM.render( < Application / > , document.getElementById('root'));
